0588227ec82beeedfb53dc953102d9d718cc06c6
LUCA AGOSTINO <agostino.luca89@gmail.com>
2024-06-12 16:42:30 +0200

    add partials for error message or confirm message add partial for check path image

---
+++ resources/views/partials/check_image.blade.php
@@ -0,0 +1,16 @@
+@php
+    $width = $width ?? '100%';
+    $aspectRatio = $aspectRatio ?? null;
+
+@endphp
+
+@if (Str::startsWith($photo->image, 'https://'))
+    <img class="rounded-3 card-img-top" src="{{ $photo->image }}" alt="Photo number {{ $photo->id }}" loading="lazy"
+        style="width: {{ $width }}; @if ($aspectRatio) aspect-ratio: {{ $aspectRatio }}; @endif ">
+@elseif (!$photo->image)
+    <div class="text-secondary">No image uploaded</div>
+@else
+    <img class="rounded-3 card-img-top" src="{{ asset('storage/' . $photo->image) }}"
+        alt="Photo number {{ $photo->id }}"
+        style="width: {{ $width }}; @if ($aspectRatio) aspect-ratio: {{ $aspectRatio }}; @endif ">
+@endif
