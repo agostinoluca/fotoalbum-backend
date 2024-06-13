48214546a95c288fbba05d532109b0db034156e7
LUCA AGOSTINO <agostino.luca89@gmail.com>
2024-06-13 17:19:02 +0200

    add one to many relationship to models

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Photo extends Model
 {
@@ -18,4 +19,14 @@ class Photo extends Model
         'published',
         'slug',
     ];
+
+    /**
+     * Get the category that owns the Photo
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
